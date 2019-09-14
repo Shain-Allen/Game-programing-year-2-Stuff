a8dbb112a92f53556a59d4f1fb65eb2e0e3c53c3
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-09-14 00:17:12 -0700

    Kill me now

--- indipented practice/Setting up openGL and making a window/OpenGL/OpenGL/src/Application.cpp
+++ indipented practice/Setting up openGL and making a window/OpenGL/OpenGL/src/Application.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
@@ -7,6 +8,7 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -18,6 +20,8 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	glewInit();
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
