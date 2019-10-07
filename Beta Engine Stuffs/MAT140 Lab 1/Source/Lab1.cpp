0b936f3f326db5ab00c24792bd6886729afedb4b
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-10-07 12:52:48 -0700

    fixed

--- Beta Engine Stuffs/MAT140 Lab 1/Source/Lab1.cpp
+++ Beta Engine Stuffs/MAT140 Lab 1/Source/Lab1.cpp
@@ -1,3 +1,14 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Lab1.cpp
+// Author(s):	Shain Allen
+// Project:		Lab1
+// Course:		MAT140
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
 #include "stdafx.h"	//precomplied header File
 #include "Lab1.h"
 
@@ -5,7 +16,7 @@ using std::endl;
 using std::cout;
 
 Lab1::Lab1()
-	:BetaObject("Lab1"), pi(float(M_PI)), lineLength(2), rotationalVelocity(1.0/60.0), origin(0,0)
+	:BetaObject("Lab1"), pi(float(M_PI)), lineLength(2), rotationalVelocity(1.0f/60.0f), origin(0,0)
 {
 	angle1 = pi / 2;
 	angle2 = pi / 2;
@@ -48,11 +59,11 @@ void Lab1::Update(float dt)
 	angle3 -= ((dt * rotationalVelocity * pi * 2) / 60) / 60;
 
 	Vector2D end = Vector2D((lineLength) * cos(angle1), (lineLength) * sin(angle1));
-	debug.AddLineToList(start, end, Color(0.5, 0, 0));
-	Vector2D end2 = Vector2D((lineLength - 0.5)* cos(angle2), (lineLength - 0.5)* sin(angle2));
-	debug.AddLineToList(start, end2, Color(0.2, 0.8, 0.2));
-	Vector2D end3 = Vector2D((lineLength - 0.75)* cos(angle3), (lineLength - 0.75)* sin(angle3));
-	debug.AddLineToList(start, end3, Color(0.1, 0.43, 0.8));
+	debug.AddLineToList(start, end, Color(0.5f, 0.0f, 0.0f));
+	Vector2D end2 = Vector2D((lineLength - 0.5f)* cos(angle2), (lineLength - 0.5f)* sin(angle2));
+	debug.AddLineToList(start, end2, Color(0.2f, 0.8f, 0.2f));
+	Vector2D end3 = Vector2D((lineLength - 0.75f)* cos(angle3), (lineLength - 0.75f)* sin(angle3));
+	debug.AddLineToList(start, end3, Color(0.1f, 0.43f, 0.8f));
 	debug.EndLineList(camera);
 
 }
