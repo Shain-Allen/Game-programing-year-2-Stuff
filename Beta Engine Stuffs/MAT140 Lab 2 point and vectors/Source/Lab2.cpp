c5277bf724e8af348c302a7399a66431b78a828d
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-10-13 14:02:18 -0700

    heck

--- Beta Engine Stuffs/MAT140 Lab 2 point and vectors/Source/Lab2.cpp
+++ Beta Engine Stuffs/MAT140 Lab 2 point and vectors/Source/Lab2.cpp
@@ -15,6 +15,7 @@
 
 using std::cout;
 using std::endl;
+using Beta::Vector2D;
 
 const Beta::Vector2D& Lab2::pointP = Animator::GetPointP();
 const Beta::Vector2D& Lab2::vectorU = Animator::GetVectorU();
@@ -53,10 +54,34 @@ void Lab2::Update(float dt)
 
 	Input& input = *EngineGetModule(Input);
 
-	if (input.CheckTriggered())
+	//check if the left arrow key is pressed
+	if (input.CheckTriggered(VK_LEFT))
 	{
-
+		if (shape == 0)
+		{
+			shape = ShapeNum - 1;
+		}
+		else
+		{
+			shape -= 1;
+		}
+		
 	}
+
+	//check if the right arrow key is pressed
+	if (input.CheckTriggered(VK_RIGHT))
+	{
+		if (shape == ShapeNum - 1)
+		{
+			shape = 0;
+		}
+		else
+		{
+			shape += 1;
+		}
+	}
+
+	shapeFunctions[shape]();
 }
 
 void Lab2::Shutdown()
@@ -66,22 +91,56 @@ void Lab2::Shutdown()
 
 void Lab2::DrawLine(const Beta::Vector2D& start, const Beta::Vector2D& end)
 {
+	using namespace Beta;
+	// Get GraphicsEngine system
+	GraphicsEngine& graphics = *EngineGetModule(GraphicsEngine);
 
+	// Get DebugDraw system
+	DebugDraw& debug = *EngineGetModule(DebugDraw);
+
+	// Store reference to default camera
+	Camera& camera = graphics.GetDefaultCamera();
+
+	debug.AddLineToList(start, end, Colors::Green);
+	debug.EndLineList(camera);
 }
 
 void Lab2::CreateTriangle()
 {
+	Vector2D pPlusV = pointP + vectorV;
+	Vector2D pPlusU = pointP + vectorU;
 
+	DrawLine(pointP, pPlusV);
+	DrawLine(pointP, pPlusU);
+	DrawLine(pPlusV, pPlusU);
 }
 
 void Lab2::CreateParallelogram()
 {
+	Vector2D pointV = pointP + vectorV;
+	Vector2D pointU = pointP + vectorU;
+	Vector2D pointE = pointP + (vectorV + vectorU);
 
+	DrawLine(pointP, pointV);
+	DrawLine(pointP, pointU);
+	DrawLine(pointV, pointE);
+	DrawLine(pointU, pointE);
 }
 
 void Lab2::CreateTriforce()
 {
+	Vector2D pointV = pointP + vectorV;
+	Vector2D pointU = pointP + vectorU;
+	Vector2D pointA = pointP + (0.5f * vectorV);
+	Vector2D pointE = pointP + (0.5f * vectorU);
+	Vector2D pointF = pointP + (vectorV + vectorU) / 2;
 
+	DrawLine(pointP, pointV);
+	DrawLine(pointP, pointU);
+	DrawLine(pointV, pointU);
+	DrawLine(pointA, pointE);
+	DrawLine(pointE, pointF);
+	DrawLine(pointA, pointF);
 }
 
 void Lab2::CreateFace()
@@ -91,7 +150,22 @@ void Lab2::CreateFace()
 
 void Lab2::CreateHexagon()
 {
+	
+	Vector2D pointV = pointP + vectorV;
+	Vector2D pointU = pointP + vectorU;
+	Vector2D pointA = pointP + ((1/3) * vectorV);
+	Vector2D pointB = pointP + ((1/3) * vectorU);
+	Vector2D pointC = pointP + ((2/3) * vectorU);
+	Vector2D pointD;
+	Vector2D pointE;
+	Vector2D pointF = pointP + ((2/3) * vectorV);
 
+	DrawLine(pointA, pointB);
+	DrawLine(pointB, pointC);
+	DrawLine(pointC, pointD);
+	DrawLine(pointD, pointE);
+	DrawLine(pointE, pointF);
+	DrawLine(pointF, pointA);
 }
 
 void Lab2::CreateDiamondCut()
