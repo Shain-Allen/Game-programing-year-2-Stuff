972405839f8e8750d672092a92dcae194ba3866c
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-10-16 18:29:55 -0700

    H

---
+++ CS170 Project 4 Inheritance/Source/Shape_With_Vertices.cpp
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Shape_With_Vertices.cpp
+// Author(s):	Shain Allen
+// Project:		project 4
+// Course:		wanic GP2
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
