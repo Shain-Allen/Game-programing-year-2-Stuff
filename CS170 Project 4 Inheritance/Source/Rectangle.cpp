e2197c8633b24089eff530fb033aac3d150db29d
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-10-21 14:57:15 -0700

    yes much progress

--- CS170 Project 4 Inheritance/Source/Rectangle.cpp
+++ CS170 Project 4 Inheritance/Source/Rectangle.cpp
@@ -1,3 +1,4 @@
+#include "Rectangle.h"
 //------------------------------------------------------------------------------
 //
 // File Name:	Rectangle.cpp
@@ -8,3 +9,22 @@
 // Copyright © 2018 DigiPen (USA) Corporation.
 //
 //------------------------------------------------------------------------------
+
+Rectangle::Rectangle(Point center_, unsigned int width_, unsigned int height_)
+	: Shape_With_Vertices(center_, 4), width(width_), height(height_)
+{
+}
+
+Rectangle::~Rectangle()
+{
+}
+
+void Rectangle::SetCenter(float x_, float y_)
+{
+	Shape_With_Vertices::SetCenter(x_, y_);
+}
+
+void Rectangle::Draw() const
+{
+
+}
--- CS170 Project 4 Inheritance/Source/Rectangle.h
+++ CS170 Project 4 Inheritance/Source/Rectangle.h
@@ -9,3 +9,20 @@
 //
 //------------------------------------------------------------------------------
 #pragma once
+#include "Shape_With_Vertices.h"
+
+class Rectangle : public Shape_With_Vertices
+{
+public:
+	Rectangle(Point center_, unsigned int width_, unsigned int height_);
+	~Rectangle();
+	void SetCenter(float x_, float y_);
+	void Draw() const;
+
+private:
+
+
+
+	unsigned int width;
+	unsigned int height;
+};
