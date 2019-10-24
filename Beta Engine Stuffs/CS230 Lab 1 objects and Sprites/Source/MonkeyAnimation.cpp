7d22e9809197c174dcb54da7aa98384c9ee176c5
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-10-24 14:28:24 -0700

    starting lab 230

---
+++ Beta Engine Stuffs/CS230 Lab 1 objects and Sprites/Source/MonkeyAnimation.cpp
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	MonkeyAnimation.cpp
+// Author(s):	Student name here
+// Project:		BetaFramework
+// Course:		WANIC VGP2 2018-2019
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"				// Precompiled header
+#include "MonkeyAnimation.h"	// Class file
+
+using namespace Beta;
+
+// STUDENT CODE GOES HERE
+
+
+
+// Create extra component functions - DO NOT REMOVE
+COMPONENT_SUBCLASS_DEFINITION(MonkeyAnimation)
