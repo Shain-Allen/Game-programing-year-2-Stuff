477dc22594867d1f9faf5439f803d721b6b2c8d3
Shain-Allen <48696021+Shain-Allen@users.noreply.github.com>
2019-10-24 19:05:17 -0700

    progress?

--- Beta Engine Stuffs/CS230 Lab 1 objects and Sprites/Source/Level1.cpp
+++ Beta Engine Stuffs/CS230 Lab 1 objects and Sprites/Source/Level1.cpp
@@ -1,6 +1,19 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Level1.cpp
+// Author(s):	Shain Allen
+// Project:	Level1.cpp
+// Course:	wanic GP2
+//
+// Copyright © 2018 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
 #include "stdafx.h"
 #include "Level1.h"
 using namespace Beta;
+using std::cout;
+using std::endl;
 
 Level1::Level1()
 	: Beta::Level("LEVEL1")
@@ -9,11 +22,23 @@ Level1::Level1()
 
 void Level1::Load()
 {
+	//creating texture for monkey
+	textureMonkey = ResourceGetTexture("Monkey.png");
+	
+	//making sprite source using monkey texture
+	spriteSourceMonkey = std::make_shared<SpriteSource>(textureMonkey, "Monkey", 3, 5);
 
 }
 
 void Level1::Initialize()
 {
+	cout << "Level1::Initialize" << endl;
+
+	//get graphics engine
+	GraphicsEngine& graphics = *EngineGetModule(GraphicsEngine);
+	graphics.SetBackgroundColor(Colors::White);
+
+	GetSpace()->GetObjectManager().AddObject(CreateMonkey());
 }
 
 void Level1::Update(float dt)
